eafc32bc4b1b5bd5d91d3eaeff91c99d98943904
choisungwook <kgg1959@naver.com>
2015-04-04 14:50:20 +0900

    Update opendir_readdir.c

--- opendir-rearddir/opendir_readdir.c
+++ opendir-rearddir/opendir_readdir.c
@@ -8,8 +8,8 @@ int main()
 	struct dirent *dir_entry;
 
 	dir_info = opendir("/tmp");
-	while(dir_info){
-		if( dir_entry = readdir(dir_info))
+	if(dir_info){
+		while( dir_entry = readdir(dir_info))
 			printf("%s\n",dir_entry->d_name);
 		closedir(dir_info);
 	}
